47247834d681062c767babdafbf0b39c0766e846
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-21 16:29:28 +0900

    feat: DB 초기 설정

---
+++ src/db/DB.cs
@@ -0,0 +1,12 @@
+﻿namespace OthelloBot.src.db
+{
+    class DB
+    {
+        private static string connStr;
+
+        public static void SetConnStr(string server, string port, string database, string uid, string pwd)
+        {
+            connStr = $"Server={server};Port={port};Database={database};Uid={uid};Pwd={pwd};CharSet=utf8;";
+        }
+    }
+}
