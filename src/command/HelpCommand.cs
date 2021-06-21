9a977b117fee9318a229f92170f533335d368a96
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-21 16:28:32 +0900

    feat: 도움말 명령어 구현

---
+++ src/command/HelpCommand.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using OthelloBot.src.embed;
+using System.Threading.Tasks;
+
+namespace OthelloBot.src.command
+{
+    public class HelpCommand : ModuleBase<ShardedCommandContext>
+    {
+        [Command("도움말")]
+        public async Task Help(params string[] args)
+        {
+            var avatarUrl = Context.Client.CurrentUser.GetAvatarUrl();
+            var helpEmbed = new HelpEmbed(avatarUrl);
+
+            await Context.Channel.SendMessageAsync(embed: helpEmbed.Build());
+        }
+    }
+}
---
+++ src/embed/HelpEmbed.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+
+namespace OthelloBot.src.embed
+{
+    internal class HelpEmbed : EmbedBuilder
+    {
+        public HelpEmbed(string avatarUrl)
+        {
+            WithColor(new Color(0xFFFFFF));
+            WithThumbnailUrl(avatarUrl);
+            AddField($"{Program.prefix}도움말", "오델로봇의 사용법을 알려줍니다.");
+        }
+    }
+}
