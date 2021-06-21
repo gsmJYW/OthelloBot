2c46a55d38f4dc4bff3fd56864136f4885b954f8
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-21 16:28:17 +0900

    feat: 게임 관련 이벤트 핸들러 모음

---
+++ src/GameEventHandler.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace OthelloBot
+{
+    internal class GameEventHandler
+    {
+        protected static DataTable GameRoomTable = new DataTable();
+
+        protected async Task _client_ReactionAdded(Cacheable<IUserMessage, ulong> entity, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            if (reaction.User.Value.IsBot)
+            {
+                return;
+            }
+
+            var message = channel.GetMessageAsync(reaction.MessageId).Result;
+
+            try
+            {
+                var embeds = message.Embeds.GetEnumerator();
+                embeds.MoveNext();
+
+                var footerText = embeds.Current.Footer.Value.Text;
+                var hostId = Convert.ToUInt64(footerText[3..]);
+
+                var GameRoom = GameRoomTable.NewRow();
+                GameRoom["channel_id"] = channel.Id;
+                GameRoom["host_id"] = hostId;
+                GameRoom["guest_id"] = reaction.UserId;
+                GameRoomTable.Rows.Add(GameRoom);
+
+                await channel.DeleteMessageAsync(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                await message.RemoveReactionAsync(reaction.Emote, reaction.User.Value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected async static void GameStart(object sender, DataRowChangeEventArgs e)
+        {
+
+        }
+    }
+}
