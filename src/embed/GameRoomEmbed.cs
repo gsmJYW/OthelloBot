7372c0d53feafd7c91e05dd1f58b9f036b140ce6
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-23 15:36:46 +0900

    feat: 설명 수정

--- src/embed/GameRoomEmbed.cs
+++ src/embed/GameRoomEmbed.cs
@@ -9,14 +9,16 @@ namespace OthelloBot.src.embed
         {
             WithColor(new Color(0xFFFFFF));
             WithTitle($"{user.Username}님의 게임");
-            WithDescription($"참여를 신청하려면 :raised_hand: 이모지를 누르세요.");
+            WithDescription(
+                @"참여를 신청하려면 :raised_hand: 이모지를 누르세요.
+                30초가 지나거나 주최자가 :negative_squared_cross_mark: 이모지를 누르면 취소됩니다."
+            );
             AddField("승", 0, true);
             AddField("패", 0, true);
             AddField("\u200B", "\u200B", true);
             AddField("승률", "0%", true);
             AddField("플레이 시간", "0분", true);
             AddField("\u200B", "\u200B", true);
-            WithFooter($"ID {user.Id}");
         }
     }
 }
