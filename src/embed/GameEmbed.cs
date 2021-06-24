a0090a93baa9098f0bb54e755cceecc85eef9736
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-24 20:14:17 +0900

    feat: 보드 출력 개선

--- src/embed/GameEmbed.cs
+++ src/embed/GameEmbed.cs
@@ -1,36 +1,54 @@
 ﻿using Discord;
 using Discord.WebSocket;
-using System.Data;
 
 namespace OthelloBot.src.embed
 {
     internal class GameEmbed : EmbedBuilder
     {
-        public GameEmbed(DataRow game)
+        public GameEmbed(Game game)
         {
-            var black = game["black"] as SocketUser;
-            var white = game["white"] as SocketUser;
-
             WithColor(new Color(0xFFFFFF));
-            WithTitle($"○ 02 __{black.Username}__ vs {white.Username} 02 ●");
-            WithDescription(
-              @"`┏━━━━━┳━━━━━┳━━━━━┳━━━━━┳━━━━━┳━━━━━┳━━━━━┳━━━━━┓`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┣━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━╋━━━━━┫`
-                `┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃     ┃`
-                `┗━━━━━┻━━━━━┻━━━━━┻━━━━━┻━━━━━┻━━━━━┻━━━━━┻━━━━━┛`");
+
+            string redName = game.red.Username;
+            string blueName = game.blue.Username;
+
+            if (game.turn == Game.Piece.Red)
+            {
+                redName = $"__{redName}__";
+            }
+            else
+            {
+                blueName = $"__{blueName}__";
+            }
+
+            WithTitle($":red_circle: {game.CountPiece(Game.Piece.Red)} {redName}\n:blue_circle: {game.CountPiece(Game.Piece.Blue)} {blueName}");
+
+            var boardString = ":black_large_square::one::two::three::four::five::six::seven::eight:";
+
+            for (int y = 0; y < 8; y++)
+            {
+                boardString += $"\n:regional_indicator_{(char)('a' + y)}:";
+
+                for (int x = 0; x < 8; x++)
+                {
+                    switch (game.board[y, x])
+                    {
+                        case Game.Piece.Red:
+                            boardString += ":red_circle:";
+                            break;
+
+                        case Game.Piece.Blue:
+                            boardString += ":blue_circle:";
+                            break;
+
+                        default:
+                            boardString += ":black_large_square:";
+                            break;
+                    }
+                }
+            }
+
+            WithDescription(boardString);
         }
     }
 }
