fad84fdc5671d91983f99d288e66820ccd914367
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-29 09:45:13 +0900

    feat: 전적 명령어 추가

---
+++ src/command/StatCommand.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using OthelloBot.src.embed;
+using System.Threading.Tasks;
+
+namespace OthelloBot.src.command
+{
+    public class StatCommand : ModuleBase<ShardedCommandContext>
+    {
+        [Command("전적")]
+        public async Task Stat(params string[] args)
+        {
+            var statEmbed = new StatEmbed(Context.User);
+            await Context.Channel.SendMessageAsync(embed: statEmbed.Build());
+        }
+    }
+}
