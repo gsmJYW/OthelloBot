9b498a58489386371fdaf47850c12692236a01f0
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-29 09:57:42 +0900

    feat: 모든 명령어에 대한 설명 추가

--- src/embed/HelpEmbed.cs
+++ src/embed/HelpEmbed.cs
@@ -8,7 +8,9 @@ namespace OthelloBot.src.embed
         {
             WithColor(new Color(0xFFFFFF));
             WithThumbnailUrl(avatarUrl);
-            AddField($"{Program.prefix}도움말", "오델로봇의 사용법을 알려줍니다.");
+            AddField($"{Program.prefix}도움말", "오델로 봇의 사용법을 알려줍니다.");
+            AddField($"{Program.prefix}게임", "게임을 열고 참여할 사람을 기다립니다.");
+            AddField($"{Program.prefix}전적", "자신의 게임 전적을 확인합니다.");
         }
     }
 }
