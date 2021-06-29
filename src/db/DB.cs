e75d156729aa4ab79478ed5cd12847dfd534d1c8
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-29 09:45:42 +0900

    feat: 유저 select, update 추가

--- src/db/DB.cs
+++ src/db/DB.cs
@@ -1,4 +1,9 @@
-﻿namespace OthelloBot.src.db
+﻿using Discord.WebSocket;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace OthelloBot.src.db
 {
     class DB
     {
@@ -7,6 +12,57 @@
         public static void SetConnStr(string server, string port, string database, string uid, string pwd)
         {
             connStr = $"Server={server};Port={port};Database={database};Uid={uid};Pwd={pwd};CharSet=utf8;";
+        }
+
+        public static DataRow GetUser(ulong user_id)
+        {
+            using var conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            string query = $"SELECT * FROM user WHERE id={user_id}";
+            MySqlDataAdapter adpt = new MySqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+            adpt.Fill(ds, "user");
+
+            try
+            {
+                return ds.Tables[0].Rows[0];
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void UpdateUser(ulong user_id, int win, int draw, int lose, int playtime_second)
+        {
+            var pre_win = 0;
+            var pre_draw = 0;
+            var pre_lose = 0;
+            var pre_playtime_second = 0;
+
+            try
+            {
+                var userRow = GetUser(user_id);
+
+                pre_win = Convert.ToInt32(userRow["win"]);
+                pre_draw = Convert.ToInt32(userRow["draw"]);
+                pre_lose = Convert.ToInt32(userRow["lose"]);
+                pre_playtime_second = Convert.ToInt32(userRow["playtime_second"]);
+            }
+            catch
+            {
+
+            }
+
+            using var conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            string query = $"REPLACE INTO user (id, win, draw, lose, playtime_second) VALUES({user_id}, {pre_win + win}, {pre_draw + draw}, {pre_lose + lose}, {pre_playtime_second + playtime_second})";
+            using var cmd = new MySqlCommand(query, conn);
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
