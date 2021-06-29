f721bbe9a5e3cd1fe0f44f65556551b0d1057c39
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-06-29 09:46:05 +0900

    feat: 전적 분리

--- src/embed/GameRoomEmbed.cs
+++ src/embed/GameRoomEmbed.cs
@@ -11,12 +11,6 @@ namespace OthelloBot.src.embed
             WithTitle($"{user.Username}님의 게임");
             WithDescription("참여를 신청하려면 :raised_hand:를 누르세요.\n30초가 지나거나 주최자가 :negative_squared_cross_mark:를 누르면 취소됩니다.");
             WithFooter($"ID {user.Id}");
-
-            AddField("승", 0, true);
-            AddField("무", 0, true);
-            AddField("패", 0, true);
-            AddField("승률", "0%", true);
-            AddField("플레이 시간", "0분", true);
         }
     }
 }
