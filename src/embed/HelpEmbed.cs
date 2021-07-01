a9a5da59dbf0269d7f7e8119abe687961e359e06
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-07-02 08:25:17 +0900

    feat: 전적 명령어 매개변수에 대한 설명 추가

--- src/embed/HelpEmbed.cs
+++ src/embed/HelpEmbed.cs
@@ -8,9 +8,11 @@ namespace OthelloBot.src.embed
         {
             WithColor(new Color(0xFFFFFF));
             WithThumbnailUrl(avatarUrl);
+
             AddField($"{Program.prefix}도움말", "오델로 봇의 사용법을 알려줍니다.");
             AddField($"{Program.prefix}게임", "게임을 열고 참여할 사람을 기다립니다.");
-            AddField($"{Program.prefix}전적", "자신의 게임 전적을 확인합니다.");
+            AddField($"{Program.prefix}전적 [사용자명]", "사용자명으로 검색된 유저의 게임 전적을 보여줍니다.\n사용자명을 생략하면 자신의 게임 전적을 보여줍니다.");
+            AddField($"{Program.prefix}리더보드", "상위 10명의 전적을 보여줍니다.");
         }
     }
 }
