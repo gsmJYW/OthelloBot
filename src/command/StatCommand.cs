a22c141b995de4ed4d9ebd03284b4ecc70adeb1a
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-07-02 08:21:56 +0900

    feat: 다른 사용자를 멘션이 아닌 사용자명으로 검색하도록 수정

--- src/command/StatCommand.cs
+++ src/command/StatCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
+using OthelloBot.src.db;
 using OthelloBot.src.embed;
 using System;
 using System.Threading.Tasks;
@@ -11,28 +12,28 @@ namespace OthelloBot.src.command
         [Command("전적")]
         public async Task Stat(params string[] args)
         {
-            var user = Context.User;
+            var username = $"{Context.User.Username}#{Context.User.Discriminator}";
 
             if (args.Length > 0)
             {
-                try
-                {
-                    var userId = Convert.ToUInt64(args[0][3..21]);
-                    user = Context.Guild.GetUser(userId);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-
-                if (user == null)
-                {
-                    await Context.Channel.SendMessageAsync("유저를 찾지 못했습니다.");
-                }
+                username = args[0];
             }
 
-            var statEmbed = new StatEmbed(user);
-            await Context.Channel.SendMessageAsync(embed: statEmbed.Build());
+            var users = DB.GetUsers(username);
+
+            if (users.Count == 0)
+            {
+                await Context.Channel.SendMessageAsync("사용자를 찾지 못했습니다.");
+            }
+            else if (users.Count == 1)
+            {
+                var statEmbed = new StatEmbed(users[0]);
+                await Context.Channel.SendMessageAsync(embed: statEmbed.Build());
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("여러 사용자가 검색 되었습니다.\n사용자명을 더 자세히 입력해주세요.");
+            }
         }
     }
 }
