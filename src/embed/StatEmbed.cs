c906cb303b563224755b4caa116f205062882ca7
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-07-02 10:44:07 +0900

    feat: 사용자명을 제목에서 footer로 옮김

--- src/embed/StatEmbed.cs
+++ src/embed/StatEmbed.cs
@@ -9,35 +9,28 @@ namespace OthelloBot.src.embed
     {
         public StatEmbed(DataRow user)
         {
-            WithTitle($"{user["name"]}");
             WithColor(new Color(0xFFFFFF));
 
-            try
-            {
-                var userRow = DB.GetUser(Convert.ToUInt64(user["id"]));
+            var userRow = DB.GetUser(Convert.ToUInt64(user["id"]));
 
-                var win = Convert.ToInt32(userRow["win"]);
-                var draw = Convert.ToInt32(userRow["draw"]);
-                var lose = Convert.ToInt32(userRow["lose"]);
-                var playtimeSecond = Convert.ToInt32(userRow["playtime_second"]);
+            var win = Convert.ToInt32(userRow["win"]);
+            var draw = Convert.ToInt32(userRow["draw"]);
+            var lose = Convert.ToInt32(userRow["lose"]);
+            var playtimeSecond = Convert.ToInt32(userRow["playtime_second"]);
 
-                var winRate = (win * 100) / (win + draw + lose);
+            var winRate = (win * 100) / (win + draw + lose);
 
-                var winRank = $"{userRow["win_rank"]}";
-                var playtimeSecondRank = $"{userRow["playtime_second_rank"]}";
+            var winRank = $"{userRow["win_rank"]}";
+            var playtimeSecondRank = $"{userRow["playtime_second_rank"]}";
 
-                AddField("승", $"{win} (#{winRank})", true);
-                AddField("무", draw, true);
-                AddField("패", lose, true);
+            AddField("승", $"{win} (#{winRank})", true);
+            AddField("무", draw, true);
+            AddField("패", lose, true);
 
-                AddField("승률", $"{winRate}%", true);
-                AddField("플레이 시간", $"{playtimeSecond / 60}분 (#{playtimeSecondRank})", true);
-            }
-            catch
-            {
-                WithDescription("플레이 기록이 없습니다.");
-                return;
-            }
+            AddField("승률", $"{winRate}%", true);
+            AddField("플레이 시간", $"{playtimeSecond / 60}분 (#{playtimeSecondRank})", true);
+
+            WithFooter($"{user["name"]}");
         }
     }
 }
